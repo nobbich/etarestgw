28255f419389c4712c01243befa83bd94b55aad6
nobbich <norbert.suter@gmx.net>
2022-12-31 01:41:33 +0100

    Update pu15_gateway.py

--- pu15_gateway.py
+++ pu15_gateway.py
@@ -1,7 +1,24 @@
 import prometheus_client as prom  #import 'prometheus_client', this is important as the python Prometheus library is called that  
 import time
 import requests
+import yaml
+import sys
 from bs4 import BeautifulSoup
+
+def config():
+    with open("config.yaml") as fileStream:
+        try:
+            config = yaml.safe_load(fileStream)
+        except yaml.YAMLError as exception:
+            print(exception)
+            sys.exit(0)
+    global url_source
+    global port_source
+    global prom_port
+    
+    url_source=config["PU15"]["url"]
+    port_source=config["PU15"]["port"]
+    prom_port=config["Server"]["port"]
 
 #here we are defining the gauge, it has only one metric, which is, for just to generate a number
 GAUGE_VORRAT = prom.Gauge('Pelletvorrat', 'Aktueller Lagerbestand Pellets')
@@ -10,7 +27,7 @@ GAUGE_VORRAT = prom.Gauge('Pelletvorrat', 'Aktueller Lagerbestand Pellets')
 
 def vorrat():
     while True:
-        response = requests.get('http://192.168.0.50:8080/user/var/40/10201/0/0/12015/')
+        response = requests.get(url_source + ":" + port_source + "/user/var/40/10201/0/0/12015/")
 
         soup = BeautifulSoup(response.text, 'xml')
         val=soup.find('value').text
@@ -20,5 +37,6 @@ def vorrat():
 
 
 if __name__ == '__main__':  #this is the closing function
-    prom.start_http_server(8000)  #and here are starting our server with the Prometheus client itself to post our results
+    config()
+    prom.start_http_server(prom_port)  #and here are starting our server with the Prometheus client itself to post our results
     vorrat()
